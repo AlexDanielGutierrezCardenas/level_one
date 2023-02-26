db2d42c0ff6894c2d148f5ce924407eb7c18164f
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2023-02-26 14:56:24 -0400

    level_one reemplace

--- one.js
+++ one.js
@@ -1,7 +1,7 @@
 //console.log("hola como estas")
 
 //funcion recorido c++
-
+console.log("factorial")
 factorial = (num) => {
     var total=1;
     for (i=1 ; i<=num ; i++){
@@ -10,3 +10,13 @@ factorial = (num) => {
     return total;
 }
 console.log(factorial(5))
+
+console.log("fibonacci")
+fibonacci = (num) => {
+    var a=1,b=0,c=0;
+    for (i = 1; i<=num;i++ ){
+        console.log(a);
+        b=a,c=b,a=b+c;
+    }
+}
+console.log(fibonacci(5))
