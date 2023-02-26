0b5c51d90b70a14d806af72df72f95a290922b10
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2023-02-26 10:59:29 -0400

    level_one part 2

--- one.js
+++ one.js
@@ -1,1 +1,12 @@
-console.log("hola como estas")
+//console.log("hola como estas")
+
+//funcion recorido c++
+
+factorial = (num) => {
+    var total=1;
+    for (i=1 ; i<=num ; i++){
+        total = total * i;
+    }
+    return total;
+}
+console.log(factorial(5))
